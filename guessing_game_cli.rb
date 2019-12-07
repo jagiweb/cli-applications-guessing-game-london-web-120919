2db5f0b28a2fd5d71375e24f76f09206a7279557
Javier Goncalves <jagi.ccs@gmail.com>
2019-12-07 03:16:33 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
   number_winner = rand(6) +1
-  input = gets.chomp
+  input = gets.chomp.to_s
   if input == "exit"
     puts "Goodbye!"
   elsif input != number_winner.to_s
